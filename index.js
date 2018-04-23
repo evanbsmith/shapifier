c98daabfdbfdadcb7fc074a8fa2acba6cffc9a68
Evan Smith <evanbsmith@gmail.com>
2018-04-23 12:26:19 -0400

    remove dep

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const program = require('commander');
 
 const shapify = require('./logic');
-const files = require('./dirpath');
 
 program
   .version('0.0.1')
@@ -15,7 +14,6 @@ program
   .description('Shapify CSV point data by adding polygon geojson field')
   .action((csvFileName, geojsonFileName, featurePropertyName, latField, longField) => {
     console.log('Shapifier Engaged!');
-    // console.log('Current Path: ', files.getCurrentDirectoryBase());
     shapify(csvFileName, geojsonFileName, featurePropertyName, latField, longField);
   });
 
